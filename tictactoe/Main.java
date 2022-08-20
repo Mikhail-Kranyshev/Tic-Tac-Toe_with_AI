b65908419e786e29eed8b9b1127df7e5a16972cb
Mikhail-Kranyshev <91139884+Mikhail-Kranyshev@users.noreply.github.com>
2022-08-21 01:27:15 +0700

    Stage 2/5: Easy does it

--- tictactoe/Main.java
+++ tictactoe/Main.java
@@ -1,41 +1,31 @@
 package tictactoe;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     static char[][] matrix = new char[3][3];
-    static int countX = 0;
-    static int countO = 0;
 
     private static void generate() {
-        System.out.print("Enter the cells: ");
-        char[] chars = new Scanner(System.in).next().toCharArray();
-        int k = 0;
         for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (chars[k] == 'X') {
-                    countX++;
-                } else if (chars[k] == 'O') {
-                    countO++;
-                }
-                matrix[i][j] = chars[k++];
-
-            }
+            Arrays.fill(matrix[i], ' ');
         }
+        print();
     }
     private static void print() {
         System.out.println("---------");
-        for (int i = 0; i < 3; i++) {
+        for (char[] chars: matrix) {
             System.out.print("|");
-            for (int j = 0; j < 3; j++) {
-                System.out.print(" " + matrix[i][j]);
+            for (char ch: chars) {
+                System.out.print(" " + ch);
             }
             System.out.println(" |");
         }
         System.out.println("---------");
     }
 
-    private static void inputCoordinates() {
+    private static boolean turnPlayer() {
         while (true) {
             System.out.print("Enter the coordinates: ");
             String[] strings = new Scanner(System.in).nextLine().split(" ");
@@ -43,15 +33,10 @@ public class Main {
                 int x = Integer.parseInt(strings[0]) - 1;
                 int y = Integer.parseInt(strings[1]) - 1;
                 if (x >= 0 && x <= 2 && y >= 0 && y <= 2) {
-                    if (matrix[x][y] == '_') {
-                        if (countX > countO) {
-                            matrix[x][y] = 'O';
-                            countO++;
-                        } else {
-                            matrix[x][y] = 'X';
-                            countX++;
-                        }
-                        return;
+                    if (matrix[x][y] == ' ') {
+                        matrix[x][y] = 'X';
+                        print();
+                        return !isGameOver();
                     } else {
                         System.out.println("This cell is occupied! Choose another one!");
                     }
@@ -62,47 +47,58 @@ public class Main {
                 System.out.println("You should enter numbers!");
             }
         }
-
     }
 
-    public static void check() {
-        for (int i = 0; i < 3; i++) {
-            if (matrix[i][0] == matrix[i][1] && matrix[i][0] == matrix[i][2] && matrix[i][0] != '_') {
-                System.out.println(matrix[i][0] + " wins");
-                return;
+    public static boolean turnComputer() {
+        System.out.println("Making move level \"easy\"");
+        Random random = new Random();
+        while (true) {
+            int x = random.nextInt(3);
+            int y = random.nextInt(3);
+            if (matrix[x][y] == ' ') {
+                matrix[x][y] = 'O';
+                print();
+                return !isGameOver();
             }
-            if (matrix[0][i] == matrix[1][i] && matrix[0][i] == matrix[2][i] && matrix[0][i] != '_') {
+        }
+    }
+
+    public static boolean isGameOver() {
+        for (int i = 0; i < 3; i++) {
+            if (matrix[i][0] == matrix[i][1] && matrix[i][0] == matrix[i][2] && matrix[i][0] != ' ') {
+                System.out.println(matrix[i][0] + " wins");
+                return false;
+            }
+            if (matrix[0][i] == matrix[1][i] && matrix[0][i] == matrix[2][i] && matrix[0][i] != ' ') {
                 System.out.println(matrix[0][i] + " wins");
-                return;
+                return false;
             }
         }
         if (((matrix[1][1] == matrix[0][0] && matrix[1][1] == matrix[2][2]) ||
                 (matrix[1][1] == matrix[0][2] && matrix[1][1] == matrix[2][0])) &&
-                matrix[1][1] != '_') {
+                matrix[1][1] != ' ') {
             System.out.println(matrix[1][1] + " wins");
-            return;
+            return false;
         }
         for (char[] chars : matrix) {
             for (char ch: chars) {
-                if (ch == '_') {
-                    System.out.println("Game not finished");
-                    return;
+                if (ch == ' ') {
+                    return true;
                 }
             }
         }
         System.out.println("Draw");
-
-
+        return false;
     }
 
 
 
     public static void main(String[] args) {
         generate();
-        print();
-        inputCoordinates();
-        print();
-        check();
+        while (true) {
+            if (turnPlayer()) break;
+            if (turnComputer()) break;
+        }
     }
 
 
